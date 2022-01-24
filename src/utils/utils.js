6ea090459d4dadec2ca482819b06f8217372ee91
Linus Linde <linus.linde@bahnhof.se>
2022-01-24 09:40:17 +0100

    fix: Add clsx-function

---
+++ src/utils/utils.js
@@ -0,0 +1,25 @@
+
+
+
+
+export const clsx = (...args) => 
+    args.map(arg => 
+        isObject(arg) ? Object.entries(arg).reduce((p, [k, v]) => `${p} ${v ? k : ""}`.trim(), "").trim()
+        : isArray(arg) ? clsx(...arg)
+        : isString(arg) ? arg.trim()
+        : ""
+    ).join(" ").trim()
+
+
+
+export const isNumber = v => typeof v === 'number' && !isNaN(v)
+export const isNull = v => v === null
+export const isUndefined = v => v === undefined
+export const isNullish = v => v == undefined
+export const isObject = v =>  !!v && typeof v === 'object' && !Array.isArray(v)
+export const isString = v =>  typeof v === 'string'
+export const isFunction = v =>  typeof v === 'function'
+export const isArray = v =>  Array.isArray(v)
+export const isBoolean = v => typeof v === 'boolean'
+export const isUpperCase = v => v === v.toUpperCase()
+export const isLowerCase = v => v === v.toLowerCase()
