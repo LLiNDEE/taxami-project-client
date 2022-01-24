98103c7e6f70faf24bb415eacc121e0dbf7198ac
Linus Linde <linus.linde@bahnhof.se>
2022-01-24 12:57:28 +0100

    fix: Start to implement yup schema validation. It is not working at the moment...

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,34 +1,42 @@
 import React from 'react'
-import { useForm, Controller } from 'react-hook-form'
+import { useForm, Controller, appendErrors } from 'react-hook-form'
 import TextField from '@mui/material/TextField'
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from 'yup'
 
 import Flex from '../components/core/Flex/Flex.jsx'
+
+const schema = yup.object().shape({
+    email: yup.string().required(),
+    password: yup.string().required()
+})
 
 const LoginForm = () => {
 
     const { handleSubmit, control } = useForm({
-        defaultValues: {
-            email: "",
-            password: "",
-        }
+        resolver: yupResolver(schema)
     })
     const onSubmit = data => console.log(data)
 
   return (
       <div className="loginForm">
+          <Flex justify="center">
+            <h2 className="loginFormTitle">Logga in</h2>
+          </Flex>
           <form onSubmit={handleSubmit(onSubmit)}>
             <div>
                 <Controller
                     name="email"
                     control={control}
-                    render={({ field }) => <TextField className="input" id="outlined-basic" label="E-post" variant="outlined" {...field} />}
+                    schema="email"
+                    render={({ field }) => <TextField type="email" className="input" id="outlined-basic" label="E-post" variant="outlined" {...field} />}
                 />
             </div>
             <div>
                 <Controller
                     name="password"
                     control={control}
-                    render={({ field }) => <TextField className="input" id="outlined-basic" label="Lösenord" variant="outlined" {...field} />}
+                    render={({ field }) => <TextField type="password" className="input" id="outlined-basic" label="Lösenord" variant="outlined" {...field} />}
                 />
             </div>
             <Flex justify="right">
