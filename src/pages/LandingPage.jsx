9e5e2653617eb28f6ddeb2fd12893b7dcfa9f121
LLiNDEE <linus.linde@bahnhof.se>
2022-01-24 19:43:35 +0100

    fix: Update landingPage with loginSchema and useLogin. Will move all the stuff about login to a seperate file instead

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,13 +1,25 @@
 import React, { useState } from 'react'
+import * as yup from 'yup'
 
 import LoginForm from '../components/LoginForm';
+import useLogin from '../api/useLogin'
 
+const loginSchema = yup.object().shape({
+  email: yup.string().email().required(),
+  password: yup.string().min(3).required()
+})
 
 const LandingPage = () => {
 
+  const { execute, isError, isSuccess, status } = useLogin()
+
   return (
-      <div>
-          <LoginForm/>
+      <div className="landingPage">
+          <LoginForm
+            schema={loginSchema}
+            submitText="Logga in"
+            onSubmit={execute}
+          />
       </div>
   );
 };
