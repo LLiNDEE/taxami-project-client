84b828576696b9925588190c59fea26e1326b3d4
LLiNDEE <linus.linde@bahnhof.se>
2022-01-24 19:42:17 +0100

    fix: Update LoginForm with the new formHandler and replace old input code with Form.Input

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,50 +1,23 @@
 import React from 'react'
-import { useForm, Controller, appendErrors } from 'react-hook-form'
-import TextField from '@mui/material/TextField'
-import { yupResolver } from "@hookform/resolvers/yup"
-import * as yup from 'yup'
+
+import Form from './Form/Form.jsx'
+import withForm from './Form/withForm'
 
 import Flex from '../components/core/Flex/Flex.jsx'
 
-const schema = yup.object().shape({
-    email: yup.string().required(),
-    password: yup.string().required()
-})
-
 const LoginForm = () => {
 
-    const { handleSubmit, control } = useForm({
-        resolver: yupResolver(schema)
-    })
-    const onSubmit = data => console.log(data)
 
   return (
       <div className="loginForm">
           <Flex justify="center">
             <h2 className="loginFormTitle">Logga in</h2>
           </Flex>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div>
-                <Controller
-                    name="email"
-                    control={control}
-                    schema="email"
-                    render={({ field }) => <TextField type="email" className="input" id="outlined-basic" label="E-post" variant="outlined" {...field} />}
-                />
-            </div>
-            <div>
-                <Controller
-                    name="password"
-                    control={control}
-                    render={({ field }) => <TextField type="password" className="input" id="outlined-basic" label="Lösenord" variant="outlined" {...field} />}
-                />
-            </div>
-            <Flex justify="right">
-                <input type="submit" value="Logga in" className="loginButton"/>
-            </Flex>
-          </form>
+
+          <Form.Input name="email" label="E-post"/>
+          <Form.Input type="password" name="password" label="Lösenord"/>
       </div>
   )
 };
 
-export default LoginForm;
+export default withForm(LoginForm);
