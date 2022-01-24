483d9b988bed8a8dc34d42394a2d37c2b075d321
Linus Linde <linus.linde@bahnhof.se>
2022-01-24 12:58:05 +0100

    fix: Add js-file for handling requests

---
+++ src/api/request.js
@@ -0,0 +1,18 @@
+import SERVER_URL from '../utils/constants'
+
+export const request = (...params) => fetch(...params).then(res => res.json()).then(data =>{
+    console.log("Data from request.js --> ", data)
+    if(!data?.success || data?.type === 'error') throw data
+
+    return data
+})
+
+export const post = (url, body, config) => request(SERVER_URL + url, {
+    ...config??null,
+    headers:{
+        'Content-Type': 'application/json',
+        ...config?.headers??null
+    },
+    method: 'POST',
+    body: JSON.stringify({...body})
+})
