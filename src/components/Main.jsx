7c960e76c4f8e8846386316e6567911b165ffd1c
Linus Linde <linus.linde@bahnhof.se>
2022-01-25 09:45:24 +0100

    fix: Add new route /login and remove the route / if the user is not logged in

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,15 +2,23 @@ import React from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 
 import LandingPage from '../pages/LandingPage.jsx';
-
+import LoginPage from '../pages/LoginPage.jsx';
 
 const Main = () => {
     return (
         <main>
             <Routes>
-                <Route
+                {/* <Route
                     path="/"
                     element={<LandingPage/>}
+                /> */}
+                <Route
+                    path="/login"
+                    element={<LoginPage/>}
+                />
+                <Route
+                    path="*"
+                    element={<Navigate to="/login" />}
                 />
             </Routes>
         </main>
