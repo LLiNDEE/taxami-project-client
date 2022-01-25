af00b0f72fbe20a4bb440cfc4a30cb41d374a51b
LLiNDEE <linus.linde@bahnhof.se>
2022-01-25 21:35:05 +0100

    fix: Add states for UserID, buildings and tasks. May have to move it from the globalProvider and into a provider that only is available when logged in

--- src/providers/GlobalProvider.jsx
+++ src/providers/GlobalProvider.jsx
@@ -1,15 +1,36 @@
-import React, { createContext, useContext, useState } from 'react'
+import React, { createContext, useContext, useEffect, useState } from 'react'
 
+import useUserData from '../api/useUserData'
 import { AUTH_STATUSES } from '../utils/constants'
 
 const contextGlobal = createContext({})
 
 const GlobalProvider = ({ children }) => {
 
+    const { execute: getTasks, isSuccess: isTaskSuccess, status, data: tasksDATA } = useUserData()
+
     const [authStatus, setAuthStatus] = useState(AUTH_STATUSES.idle)
 
+    const [userID, setUserID] = useState(undefined)
+    const [buildings, setBuildings] = useState(undefined)
+    const [tasks, setTasks] = useState(undefined)
+
+    useEffect(() => {
+        if(!userID || !buildings) return
+
+        getTasks({user_id: userID})
+
+    },[userID, buildings])
+
+    useEffect(() => {
+        if(!tasksDATA) return
+
+        setTasks(tasksDATA.data.tasks)
+
+    },[tasksDATA])
+
     return (
-        <contextGlobal.Provider value={{ setAuthStatus, authStatus }}>
+        <contextGlobal.Provider value={{ setAuthStatus, authStatus, setUserID, userID, buildings, setBuildings, setTasks, tasks }}>
             <div className="page">
                 {children}
             </div>
