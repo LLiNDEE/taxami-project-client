9dd229899ab16633f96d1c7af64f71ce3fb9d71c
Linus Linde <linus.linde@bahnhof.se>
2022-01-25 10:58:24 +0100

    refactor: Just change so every attribute on input is on seperate rows

--- src/components/Form/Input.jsx
+++ src/components/Form/Input.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Controller } from 'react-hook-form'
 import TextField from '@mui/material/TextField'
 
 const Input = ({ label, name, control, ...props }) => {
+
     return(
         <Controller
             name={name}
             control={control}
             defaultValue=""
-            render={({ field }) => <TextField type={props?.type} className="input" id="outlined-basic" label={label} variant="outlined" {...props} {...field }/>}
+            render={({ field }) => <TextField 
+                                        type={props?.type} 
+                                        className="input" 
+                                        id="outlined-basic" 
+                                        label={label} 
+                                        variant="outlined"
+                                        {...props} 
+                                        {...field}/>}
         />
     )
 };
