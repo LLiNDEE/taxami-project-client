d36037bdf1241d7e7a3cf311ec26d4dca4f0d537
LLiNDEE <linus.linde@bahnhof.se>
2022-01-25 21:33:57 +0100

    feat: Add loadingSpinner when form-state is pending

--- src/components/Form/FormProvider.jsx
+++ src/components/Form/FormProvider.jsx
@@ -1,10 +1,13 @@
-import React, { createContext, useContext, useEffect } from 'react';
+import React, { createContext, useContext, useEffect, useMemo } from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
 import { useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup';
 
 import Flex from '../core/Flex/Flex'
 import ErrorMessage from '../Alerts/ErrorMessage';
 import Submit from './Adornment/Submit'
+import { STATUS } from '../../hooks/useAsync'
+import { clsx } from '../../utils/utils'
 
 const contextForm = createContext({})
 
@@ -16,11 +19,20 @@ export const FORM_STATE = {
     SUCCESS: 'SUCCESS'
 }
 
+const resolveFormState = status => (
+    status === STATUS.PENDING ? FORM_STATE.LOADING
+    : status === STATUS.ERROR ? FORM_STATE.ERROR
+    : status === STATUS.SUCCESS ? FORM_STATE.SUCCESS
+    : FORM_STATE.IDLE
+)
+
 const FormProvider = ({ children, submitText, onSubmit, schema, status, feedback }) => {
 
     const { handleSubmit, control, reset, formState: { errors } } = useForm({
         resolver: yupResolver(schema)
     })
+
+    const formState = useMemo(() => resolveFormState(status), [status])  
 
     useEffect(() => {
         if(status !== FORM_STATE.ERROR) return
@@ -33,17 +45,22 @@ const FormProvider = ({ children, submitText, onSubmit, schema, status, feedback
 
   return (
       <contextForm.Provider value={{ control, errors }}>
-          <form onSubmit={handleSubmit(onSubmitTest)}>
-              
-            {children}
-
-            {status === FORM_STATE.ERROR && <ErrorMessage message={feedback ?? "Någonting gick fel..."} />}
-
-            <Flex justify="right">
-                {/* <input type="submit" value={submitText}/> */}
-                <Submit>{submitText}</Submit>
-            </Flex>
-          </form>
+        <div style={{position: "relative"}}>
+            <div className={clsx({ lowerOpacity: formState === FORM_STATE.LOADING })}>
+                <form onSubmit={handleSubmit(onSubmitTest)}>
+                    
+                    {children}
+        
+                    {formState === FORM_STATE.ERROR && <ErrorMessage message={feedback ?? "Någonting gick fel..."} />}
+        
+                    <Flex justify="right">
+                        {/* <input type="submit" value={submitText}/> */}
+                        <Submit>{submitText}</Submit>
+                    </Flex>
+                </form>
+            </div>
+            {formState === FORM_STATE.LOADING &&  <CircularProgress className="loadingSpinner" />}
+        </div>
       </contextForm.Provider>
   )
 };
