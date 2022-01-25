4a3ae247d3ce28236942c6612687a78df68be077
LLiNDEE <linus.linde@bahnhof.se>
2022-01-25 21:32:07 +0100

    fix: Add setUserID and setBuildings from the globalProvider

--- src/api/useLogin.jsx
+++ src/api/useLogin.jsx
@@ -3,20 +3,26 @@ import React, { useEffect } from 'react'
 import useAsync from '../hooks/useAsync'
 import useAuth from '../hooks/useAuth'
 import { post } from './request'
+import { useGlobal } from '../providers/GlobalProvider'
 
 const login = params => post('/user/login', params)
 
 const useLogin = () => {
     const { isSuccess, data, ...props } = useAsync(login)
+    
+    const { setUserID, setBuildings } = useGlobal()
 
     const auth = useAuth()
 
     useEffect(() => {
         if(!isSuccess) return
 
-
         console.log("SUCCESSFULL LOGIN", data)
         auth.logInUser(data.data)
+
+        setUserID(data.data.user_id)
+        setBuildings(data.data.memberOf)
+
     },[isSuccess])
 
     return { isSuccess, data, ...props }
