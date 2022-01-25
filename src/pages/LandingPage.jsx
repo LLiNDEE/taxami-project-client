365d46a0831a3d6b047a46b4bf65c47b72a5949c
Linus Linde <linus.linde@bahnhof.se>
2022-01-25 09:46:07 +0100

    fix(LoginPage): Move everything about login from landingPage into LoginPage instead

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,27 +1,8 @@
 import React, { useState } from 'react'
-import * as yup from 'yup'
-
-import LoginForm from '../components/LoginForm';
-import useLogin from '../api/useLogin'
-
-const loginSchema = yup.object().shape({
-  email: yup.string().email().required(),
-  password: yup.string().min(3).required()
-})
 
 const LandingPage = () => {
 
-  const { execute, isError, isSuccess, status } = useLogin()
 
-  return (
-      <div className="landingPage">
-          <LoginForm
-            schema={loginSchema}
-            submitText="Logga in"
-            onSubmit={execute}
-          />
-      </div>
-  );
 };
 
 export default LandingPage;
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import * as yup from 'yup'
+
+import LoginForm from '../components/LoginForm';
+import useLogin from '../api/useLogin'
+import { SERVER_ERROR_MESSAGES } from '../utils/constants'
+
+const loginSchema = yup.object().shape({
+  email: yup.string().email().required(),
+  password: yup.string().min(3).required()
+})
+
+const LoginPage = () => {
+
+    const { execute, isError, isSuccess, status } = useLogin()
+
+    return (
+        <div className="landingPage">
+            <LoginForm
+              schema={loginSchema}
+              isError={isError}
+              status={status}
+              submitText="Logga in"
+              onSubmit={execute}
+              feedback={SERVER_ERROR_MESSAGES['invalidCredentials']}
+            />
+        </div>
+    )
+
+}
+
+export default LoginPage
+
