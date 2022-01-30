2634373bd7ae47b6d44684138ed703382ed99e7c
LLiNDEE <linus.linde@bahnhof.se>
2022-01-30 21:20:51 +0100

    fix: Update main to use DataProvider when logged in

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,6 +4,7 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import LoggedInPage from '../pages/LoggedInPage.jsx';
 import LoginPage from '../pages/LoginPage.jsx';
 import Buildings from '../pages/Buildings.jsx';
+import DataProvider from '../providers/DataProvider.jsx';
 import { useGlobal } from '../providers/GlobalProvider.jsx';
 import { AUTH_STATUSES } from '../utils/constants.js';
 
@@ -24,21 +25,23 @@ const Main = () => {
                     element={<Navigate to="/loggain" />}
                 />
             </Routes>
-            : authStatus === AUTH_STATUSES.loggedIn ? 
-            <Routes>
-                <Route
-                    path="/oversikt"
-                    element={<LoggedInPage/>}
-                />
-                <Route
-                    path="/byggnader"
-                    element={<Buildings/>}
-                />
-                <Route
-                    path="*"
-                    element={<Navigate to="/oversikt"/>}
-                />
-            </Routes>
+            : authStatus === AUTH_STATUSES.loggedIn ?
+            <DataProvider>
+                <Routes>
+                    <Route
+                        path="/oversikt"
+                        element={<LoggedInPage/>}
+                    />
+                    <Route
+                        path="/byggnader"
+                        element={<Buildings/>}
+                    />
+                    <Route
+                        path="*"
+                        element={<Navigate to="/oversikt"/>}
+                    />
+                </Routes>
+            </DataProvider>
             : null
             }
         </main>
