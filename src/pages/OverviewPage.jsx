c4b93810f3152c5fdd6c0135cf775c2c91998c13
Linus Linde <linus.linde@bahnhof.se>
2022-01-31 09:44:07 +0100

    fix: Remove loggedInPage and replace it with OverviewPage instead

---
+++ src/pages/OverviewPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Overview from '../components/overview/Overview'
+
+const OverviewPage = () => {
+    
+    return (
+        <>
+            <Overview/>
+        </>
+    )
+}
+
+export default OverviewPage
+
