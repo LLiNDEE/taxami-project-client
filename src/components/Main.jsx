c8dde7b21dba214d65052c0ec9d970f39b7d0ae9
LLiNDEE <linus.linde@bahnhof.se>
2022-02-01 22:24:50 +0100

    feat: Add new route /registrera

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,8 +4,10 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import LoginPage from '../pages/LoginPage.jsx';
 import OverviewPage from '../pages/OverviewPage'
 import Buildings from '../pages/Buildings.jsx';
+import RegisterPage from '../pages/RegisterPage.jsx';
 import useAuth from '../hooks/useAuth.jsx';
 import DataProvider from '../providers/DataProvider.jsx';
+import RegisterProvider from '../providers/RegisterProvider.jsx';
 import { useGlobal } from '../providers/GlobalProvider.jsx';
 import { AUTH_STATUSES } from '../utils/constants.js';
 
@@ -38,6 +40,10 @@ const Main = () => {
                 <Route
                     path="*"
                     element={<Navigate to="/loggain" />}
+                />
+                <Route
+                    path="/registrera"
+                    element={<RegisterProvider><RegisterPage/></RegisterProvider>}
                 />
             </Routes>
             : authStatus === AUTH_STATUSES.loggedIn ?
