97d7e6b1f43f762b427e6df08a2dcfea9ba70aaf
LLiNDEE <linus.linde@bahnhof.se>
2022-02-01 20:11:01 +0100

    feat: Add new accordion-taskList. Basically the same as TasksList-component, but it is made with an accordion that displays more detailed information about the task when clicked. Also works with the icon-buttons.

---
+++ src/components/Lists/TaskListWithAccordion/TaskListWithAccordion.jsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import CancelIcon from '@mui/icons-material/Cancel';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+import { useGlobal } from '../../../providers/GlobalProvider'
+import { useData } from '../../../providers/DataProvider';
+
+
+import List from '../List';
+
+import './TaskListWithAccordion.scss'
+
+import { clsx } from '../../../utils/utils'
+
+const PRIORITIES = {
+    low: 'low',
+    medium: 'medium',
+    high: 'high'
+  }
+  
+  const STATUSES = {
+    idle: 'idle',
+    inProgress: 'inProgress',
+    completed: 'completed',
+  }
+  
+  const resolvePriority = priority => 
+  priority === PRIORITIES.low ? "Låg"
+  : priority === PRIORITIES.medium ? "Medium"
+  : "Hög"
+  
+  const resolveStatus = status =>
+  status === STATUSES.inProgress ? 'Pågående'
+  : status === STATUSES.idle ? "Inte tilldelad" 
+  : "Klar"
+
+const TaskListWithAccordion = ({ 
+    myTasks, 
+    withEye, 
+    withAcceptDenyIcons,
+    withAcceptIcon,
+    withDenyIcon,
+    withAssignIcon,
+    ...props 
+  }) => {
+
+    const { userID } = useGlobal()
+    const { markTaskAsComplete } = useData()
+
+
+    return (
+        <List
+        keys={['Titel', 'Prioritet', 'Status']}
+        {...props}
+        >
+        {myTasks.map(task => (
+        <Accordion key={task._id}>
+            <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+            >
+                <div className="dataAccordion">
+                    <p className="dataItem ">{task.title}</p>
+                    <p className="dataItem  ">{resolvePriority(task.priority)}</p>
+                    <p className="dataItem ">
+                        <span 
+                            className={clsx({
+                            status: true,
+                            [`status--${task.status}`]: true
+                            })}
+                            >
+                            {resolveStatus(task.status)}
+                        </span>
+                    </p>
+                </div>
+            </AccordionSummary>
+            <AccordionDetails>
+                <div className="taskDetails">
+                    <p className="detailsItem">Förväntad tid: {task.details.estimated_time} minuter</p>
+                    <p className="detailsItem">Förväntad kostnad: {task.details.estimated_cost} kr</p>
+                </div>
+                <p className="optionalComment">Övrig kommentar: {task.details.optional_comment}</p>
+                <div className="buttons">
+                    {withAcceptDenyIcons && 
+                        <>
+                            <p className="iconText denyIcon"><CancelIcon/> Lämna uppgift</p>
+                            <p className="iconText acceptIcon" onClick={() => markTaskAsComplete({user_id: userID, task_id: task._id})}><CheckCircleIcon/> Markera som klar</p>
+                        </>
+                    }
+                    {withDenyIcon && <p className="denyIcon iconText"><CancelIcon/> Ta bort klar markering</p>}
+                </div>
+            </AccordionDetails>
+        </Accordion>
+        ))}
+      </List>
+    )
+
+};
+
+export default TaskListWithAccordion;
