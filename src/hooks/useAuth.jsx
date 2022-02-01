9fccf3cc69bcaeb7015bb153b53f02f3a5919cfa
Linus Linde <linus.linde@bahnhof.se>
2022-02-01 15:12:32 +0100

    fix: Add functions for validating the user session, to logout a user and to refresh the uuid. Because on refresh the state that handles the userID gets reset and therefore it needs to be refreshed by the userID in the token

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -1,11 +1,29 @@
 import { useMemo, useEffect } from 'react'
+import jwt_decode from 'jwt-decode'
 
 import { useGlobal } from '../providers/GlobalProvider'
 import { useData } from '../providers/DataProvider'
 import { AUTH_STATUSES } from '../utils/constants'
+import { convertMicroToMilli } from '../utils/utils'
 
 const setToken = token => sessionStorage.setItem('token', token)
+const removeToken = () => sessionStorage.removeItem('token')
 const getToken = () => sessionStorage.getItem('token')
+
+export const validateToken = () => {
+    const token = getToken()
+
+    if(!token) return false
+
+    const decodedToken = jwt_decode(token)
+    if(!decodedToken) return false
+
+    const timeInMicroseconds = Date.now()
+    const timeInMilliseconds = convertMicroToMilli(timeInMicroseconds)
+    if(timeInMilliseconds > decodedToken.exp) return false
+    
+    return true
+}
 
 const useAuth = () => {
 
@@ -17,6 +35,20 @@ const useAuth = () => {
             setToken(token)
             setAuthStatus(AUTH_STATUSES.loggedIn)
             setUserID(user_id)
+        },
+        logoutUser: () => {
+            setAuthStatus(AUTH_STATUSES.loggedOut)
+            setUserID(undefined)
+            removeToken()
+        },
+        validateSession: () => {
+            if(validateToken) return true
+
+            return false
+        },
+        refreshUUID: () => {
+            const decodedToken = jwt_decode(getToken())
+            setUserID(decodedToken.data.uuid)
         }
     }), [])
 
