e2370d323664ca7b6bb758d3915ed36e1e7aeca6
Linus Linde <linus.linde@bahnhof.se>
2022-02-01 15:12:54 +0100

    feat: Add logout button. No style at the moment

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 
 import './Header.scss'
 
+import useAuth from '../../hooks/useAuth'
 
 const Header = () => {
+
+    const auth = useAuth()
+
     return (
         <header>
             <h1>Logged in header</h1>
+            <button onClick={() => auth.logoutUser()}>Logga ut</button>
         </header>
     )
 }
