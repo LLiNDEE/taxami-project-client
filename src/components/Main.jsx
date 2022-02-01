48453355c2eaab99d658bdecf3475c11823945a4
Linus Linde <linus.linde@bahnhof.se>
2022-02-01 15:13:29 +0100

    fix: Add useEffect that validates the session on mount

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 
 import LoginPage from '../pages/LoginPage.jsx';
 import OverviewPage from '../pages/OverviewPage'
 import Buildings from '../pages/Buildings.jsx';
+import useAuth from '../hooks/useAuth.jsx';
 import DataProvider from '../providers/DataProvider.jsx';
 import { useGlobal } from '../providers/GlobalProvider.jsx';
 import { AUTH_STATUSES } from '../utils/constants.js';
 
+
 const Main = () => {
 
-    const { authStatus } = useGlobal()
+    const auth = useAuth()
+
+    const { authStatus, setAuthStatus } = useGlobal()
+
+    useEffect(async () => {
+        if(auth.validateSession()){
+            auth.refreshUUID()
+            setAuthStatus(AUTH_STATUSES.loggedIn)
+            console.log("Hello world")
+            return
+        }else{
+            auth.logoutUser()  
+        } 
+    },[])
 
     return (
         <main>
