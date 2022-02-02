fcdc5b3a02b70c65cc99f76732e8310368ce5310
LLiNDEE <linus.linde@bahnhof.se>
2022-02-02 17:11:26 +0100

    fix: Add a hook that handles modals. Stores the variant of the currently active modal, aslo stores the status of the modal, either SHOWN or HIDDEN

---
+++ src/hooks/useModal.jsx
@@ -0,0 +1,33 @@
+import React, { useCallback, useState } from 'react';
+
+const STATUS = {
+    HIDDEN: 'HIDDEN',
+    SHOWN: 'SHOWN'
+}
+
+const VARIANTS = {
+    leaveTask: 'leaveTask',
+    takeTask: 'takeTask',
+    completeTask: 'completeTask',
+}
+
+const useModal = () => {
+
+    const [variant, setVariant] = useState(undefined)
+    const [status, setStatus] = useState(undefined)
+
+    const showVariant = useCallback(params => {
+        if(!VARIANTS[params]) return
+        setVariant(params)
+        setStatus(STATUS.SHOWN)
+    }, [])
+
+    const hideVariant = useCallback(() => {
+        setVariant(undefined)
+        setStatus(STATUS.HIDDEN)
+    }, [])
+
+  return {showVariant, hideVariant, variant, status};
+};
+
+export default useModal;
