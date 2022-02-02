e8f12116515ffa9ccd03775e8ff32e2f35421d11
Linus Linde <linus.linde@bahnhof.se>
2022-02-02 11:07:05 +0100

    feat(TakeTaskForm): Move the file into a new folder. Implemented mui-inputs and connected those to states stored in the dataProvider

---
+++ src/components/TakeTaskForm/TakeTaskForm.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import './TakeTaskForm.scss'
+
+import Input from '../core/Input/Input'
+
+const TakeTaskForm = ({ takeTaskDetails, setTakeTaskDetails }) => {
+
+
+    return (
+        <div className="takeTaskForm">
+            <Input label="Förväntad kostnad" value={takeTaskDetails.estimated_cost} onChange={(e) => setTakeTaskDetails(v => ({...v, estimated_cost: e.target.value}))} />
+            <Input label="Förväntad tid" value={takeTaskDetails.estimated_time} onChange={(e) => setTakeTaskDetails(v => ({...v, estimated_time: e.target.value}))} />
+            <Input label="Valfri kommentar" value={takeTaskDetails.optional_comment} onChange={(e) => setTakeTaskDetails(v => ({...v, optional_comment: e.target.value}))} multiline />
+        </div>
+    )
+}
+
+export default TakeTaskForm
