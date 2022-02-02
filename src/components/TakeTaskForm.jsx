b418c0333a9cd647fa1d22ed21e28a60187866cb
Linus Linde <linus.linde@bahnhof.se>
2022-02-02 09:59:05 +0100

    feat: Start to work on the form that handles take task

---
+++ src/components/TakeTaskForm.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Input from './Form/Fields/Input'
+
+const TakeTaskForm = () => {
+
+
+    return (
+        <div className="taskTaskForm">
+            <Input label="Förvantad kostnad" name="estimated_cost" />
+        </div>
+    )
+}
+
+export default TakeTaskForm
