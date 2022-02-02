0b2ecacdd6d4dc2b817f7b9262283d158da0135b
LLiNDEE <linus.linde@bahnhof.se>
2022-02-02 17:12:04 +0100

    refactor: Rename takeTaskDetails to just data

--- src/components/TakeTaskForm/TakeTaskForm.jsx
+++ src/components/TakeTaskForm/TakeTaskForm.jsx
@@ -4,14 +4,14 @@ import './TakeTaskForm.scss'
 
 import Input from '../core/Input/Input'
 
-const TakeTaskForm = ({ takeTaskDetails, setTakeTaskDetails }) => {
+const TakeTaskForm = ({ data, setData }) => {
 
 
     return (
         <div className="takeTaskForm">
-            <Input label="Förväntad kostnad (kronor)" value={takeTaskDetails.estimated_cost} onChange={(e) => setTakeTaskDetails(v => ({...v, estimated_cost: e.target.value}))} />
-            <Input label="Förväntad tid (minuter)" value={takeTaskDetails.estimated_time} onChange={(e) => setTakeTaskDetails(v => ({...v, estimated_time: e.target.value}))} />
-            <Input label="Valfri kommentar" value={takeTaskDetails.optional_comment} onChange={(e) => setTakeTaskDetails(v => ({...v, optional_comment: e.target.value}))} multiline />
+            <Input label="Förväntad kostnad (kronor)" value={data.estimated_cost} onChange={(e) => setData(v => ({...v, estimated_cost: e.target.value}))} />
+            <Input label="Förväntad tid (minuter)" value={data.estimated_time} onChange={(e) => setData(v => ({...v, estimated_time: e.target.value}))} />
+            <Input label="Valfri kommentar" value={data.optional_comment} onChange={(e) => setData(v => ({...v, optional_comment: e.target.value}))} multiline />
         </div>
     )
 }
