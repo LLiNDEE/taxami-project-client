94bd6bf20bdc61523966e63fb00f1cbee920f614
LLiNDEE <linus.linde@bahnhof.se>
2022-02-03 22:02:40 +0100

    fix: Add imports of add task hooks. Might move all the modal handling into another file instead...

--- src/providers/DataProvider.jsx
+++ src/providers/DataProvider.jsx
@@ -5,6 +5,7 @@ import LeaveTaskModal from '../components/Modals/leaveTask/LeaveTaskModal';
 import TakeTaskModal from '../components/Modals/takeTask/TakeTaskModal';
 import TaskRemoveCompletedModal from '../components/Modals/taskRemoveCompleted/TaskRemoveCompletedModal';
 import CompleteTaskModal from '../components/Modals/completeTask/CompleteTaskModal'
+import AddTaskModal from '../components/Modals/addTask/AddTaskModal';
 import useUserData from '../api/useUserData'
 import useUserBuildings from '../api/useUserBuildings';
 import useTaskComplete from '../api/useTaskComplete';
@@ -12,6 +13,7 @@ import useUserTakeTask from '../api/useUserTakeTask';
 import useModal from '../hooks/useModal'
 import useTaskUpdate from '../api/useTaskUpdate';
 import useUserLeaveTask from '../api/useUserLeaveTask';
+import useAddTask from '../api/useAddTask'
 import useBoolean from '../hooks/useBoolean'
 import { useGlobal } from './GlobalProvider';
 
@@ -27,6 +29,7 @@ const DataProvider = ({ children }) => {
     const { execute: leaveTask, isSuccess: leaveTaskSuccess, data: leaveTaskData, isError: leaveTaskError } = useUserLeaveTask()
     const { execute: takeTask, iSuccess: takeTaskSuccess, data: takeTaskData, isError: takeTaskError } = useUserTakeTask()
     const { execute: updateTask, isSuccess: updateTaskSuccess, data: updateTaskData, isError: updateTaskError } = useTaskUpdate()
+    const { execute: addTask, isSuccess: addTaskSuccess } = useAddTask()
 
     const { showVariant: showModalVariant, hideVariant: hideModal, variant: modalVariant, status: modalStatus } = useModal()
 
@@ -97,14 +100,23 @@ const DataProvider = ({ children }) => {
 
     },[updateTaskData])
 
+    useEffect(() => {
+        if(!addTaskSuccess) return
+
+        hideModal()
+        setRefreshPage(true)
+
+    },[addTaskSuccess])
+
   return (
-      <contextData.Provider value={{ buildings, setBuildings, myTasks, setMyTasks, isDataLoading, markTaskAsComplete, setSelectedTaskID, setSelectedBuildingID, selectedBuildingID, selectedTaskID, setRefreshPage, refreshPage, hideModal, showModalVariant, leaveTask, takeTask, updateTask }}>
+      <contextData.Provider value={{ buildings, setBuildings, myTasks, setMyTasks, isDataLoading, markTaskAsComplete, setSelectedTaskID, setSelectedBuildingID, selectedBuildingID, selectedTaskID, setRefreshPage, refreshPage, hideModal, showModalVariant, leaveTask, takeTask, updateTask, addTask }}>
           <Header/>
             { modalStatus === 'SHOWN' && <div className="dataProviderPageCover" onClick={hideModal} ></div>}
             { modalVariant === 'takeTask' && <TakeTaskModal/>}
             { modalVariant === 'leaveTask' && <LeaveTaskModal/>}
             { modalVariant === 'completeTask' && <CompleteTaskModal/>}
             { modalVariant === 'removeCompletedTask' && <TaskRemoveCompletedModal/> }
+            { modalVariant === 'addTask' && <AddTaskModal/> }
 
           {children}
       </contextData.Provider>
