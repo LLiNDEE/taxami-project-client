39c81c3440ee4069d2a8551cbe992a194292be9c
Linus Linde <linus.linde@bahnhof.se>
2022-02-03 10:57:36 +0100

    fix: Add modal for removing the completed tag on the task

---
+++ src/components/Modals/taskRemoveCompleted/TaskRemoveCompletedModal.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+import Modal from '../../core/Modal/Modal'
+import { useGlobal } from '../../../providers/GlobalProvider';
+import { useData } from '../../../providers/DataProvider';
+
+
+const TaskRemoveCompletedModal = () => {
+
+    const { userID } = useGlobal()
+    const { selectedTaskID, hideModal, updateTask  } = useData()
+    
+
+  return (
+    <Modal
+        variant="yesNO"
+        modalTitle="Är du säker på att du vill ta bort klarmarkeringen?"
+        content={<p>Genom att ta bort klarmarkeringen kommer statusen att ändras till Pågående.</p>}
+        yesText="Ta bort klarmarkering"
+        noText="Avbryt"
+        cancelFunc={() => hideModal()}
+        acceptFunc={() => updateTask({user_id: userID, task_id: selectedTaskID, status: 'inProgress'})}
+    /> 
+  );
+};
+
+export default TaskRemoveCompletedModal;
