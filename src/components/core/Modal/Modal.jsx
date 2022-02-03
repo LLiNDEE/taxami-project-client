6fb655a7c53c91146dd6efefc1f2d2bb84a91915
LLiNDEE <linus.linde@bahnhof.se>
2022-02-03 22:01:08 +0100

    feat: Add variants default and noButton

--- src/components/core/Modal/Modal.jsx
+++ src/components/core/Modal/Modal.jsx
@@ -7,7 +7,9 @@ import './Modal.scss'
 import { clsx } from '../../../utils/utils'
 
 const VARIANTS = {
+    noButton: 'noButton',
     yesNO: 'yesNO',
+    default: 'default',
 }
 
 const Modal = ({ modalTitle, yesText, noText, cancelFunc, acceptFunc, content, variant = "yesNO" }) => {
@@ -17,6 +19,9 @@ const Modal = ({ modalTitle, yesText, noText, cancelFunc, acceptFunc, content, v
         <div className="modal">
             {modalTitle && <h2 className="modalTitle">{modalTitle}</h2>}
             {content && content}
+            {variant === VARIANTS.noButton &&
+                <p className="iconWithText cancelIcon" onClick={cancelFunc}><CancelIcon/>{noText ?? "Avbryt"}</p>
+            }
             {variant === VARIANTS.yesNO && 
                 <div className="buttons">
                     <p className="iconWithText cancelIcon" onClick={cancelFunc}><CancelIcon/>{noText ?? "Avbryt"}</p>
