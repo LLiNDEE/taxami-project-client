ddcabb39ef743bd559011a1212a3732c80d9ae0d
Linus Linde <linus.linde@bahnhof.se>
2022-02-04 09:24:58 +0100

    fix: Implement useQuery. When the user verify the code is pushed to the url. If the user gets a link with ?code it will automatically verify that code. Easier for workers to just get the link with the code, instead of having to go to the website and then verify and then register

--- src/providers/RegisterProvider.jsx
+++ src/providers/RegisterProvider.jsx
@@ -1,28 +1,42 @@
-import React, { createContext, useContext, useEffect, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState, useMemo } from 'react';
 
 import useVerifyCode from '../api/useVerifyCode';
 import useRegister from '../api/useRegister';
 import useBoolean from '../hooks/useBoolean';
+import useQuery from '../hooks/useQuery';
 
 const contextRegister = createContext({})
 
 const RegisterProvider = ({ children }) => {
 
+    const { params, push } = useQuery()
+
     const {execute: verifyCode, isSuccess: isCodeSuccess, isError: verifyCodeError, status: verifyCodeStatus, data: codeData} = useVerifyCode()
     const {execute: register, isSuccess: registerSuccess, isError: registerError, status: registerStatus } = useRegister()
 
-    const [code, setCode] = useState(undefined)
     const [isCodeValid, { on: codeIsValid, off: codeNotValid }] = useBoolean(undefined)
     const [isRegistered, { set: setIsRegistered }] = useBoolean(undefined)
 
     const [activeStep, setActiveStep] = useState(0)
 
+    const code = useMemo(() => params.get('code'), [isCodeValid])
+
+    useEffect(() => {
+        const checkCode = params.get('code')
+        if(!code) return
+
+        verifyCode({code: checkCode})
+
+    },[])
+
     useEffect(() => {
         if(!isCodeSuccess) return
 
         codeIsValid()
-        setCode(codeData.data.code)
         setActiveStep(1)
+
+        params.set("code", codeData.data.code)
+        push()
 
     },[isCodeSuccess])
 
@@ -42,7 +56,7 @@ const RegisterProvider = ({ children }) => {
     },[registerSuccess])
 
   return (
-      <contextRegister.Provider value={{ setCode, code, isCodeValid, verifyCode, verifyCodeError, verifyCodeStatus, register, registerError, registerStatus, isRegistered, activeStep }}>
+      <contextRegister.Provider value={{ code, isCodeValid, verifyCode, verifyCodeError, verifyCodeStatus, register, registerError, registerStatus, isRegistered, activeStep }}>
           {children}
       </contextRegister.Provider>
   );
