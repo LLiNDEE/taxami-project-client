b218d60185b109cb9570337b2b4e71c06eab5cfc
Linus Linde <linus.linde@bahnhof.se>
2022-02-15 11:12:34 +0100

    fix: Add check that renders different overviews depending on the user role

--- src/pages/OverviewPage.jsx
+++ src/pages/OverviewPage.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 
 import Overview from '../components/overview/Overview'
+import AdminOverview from '../components/adminOverview/AdminOverview'
+import AdminProvider from '../providers/AdminProvider'
+import { useGlobal } from '../providers/GlobalProvider'
 
 const OverviewPage = () => {
+
+    const { userRole } = useGlobal()
     
     return (
         <>
-            <Overview/>
+            {userRole === 'admin' ?
+                <AdminProvider>
+                    <AdminOverview/>
+                </AdminProvider>
+            : 
+                <Overview/>
+            }
         </>
     )
 }
