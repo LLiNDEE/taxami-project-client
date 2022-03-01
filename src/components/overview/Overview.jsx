85ff462c64435f0d9b9bca79281bc274644d54a9
LLiNDEE <linus.linde@bahnhof.se>
2022-03-01 19:53:13 +0100

    feat: Fix the overview-page so it renders different components depending on if it's mobile-view or not. On mobile-view it displays the cardList instead of the normal list with accordion

--- src/components/overview/Overview.jsx
+++ src/components/overview/Overview.jsx
@@ -11,13 +11,20 @@ import TaskListWithAccordion from '../Lists/TaskListWithAccordion/TaskListWithAc
 import TasksList from '../Lists/TasksList/TasksList';
 import { useData } from '../../providers/DataProvider';
 import { useGlobal } from '../../providers/GlobalProvider';
+import { clsx } from '../../utils/utils';
 
+import BuildingCardList from './buildingCardList/BuildingCardList';
+import TaskCardList from './TaskCardList/TaskCardList';
+import useBreakpoint from '../../hooks/useBreakpoint';
 
 
 const Overview = () => {
 
     const { userRole } = useGlobal()
     const { buildings, myTasks, isDataLoading, showModalVariant } = useData()
+
+    const { sm } = useBreakpoint()
+    console.log("SM --> ", sm)
 
   return (
     <div className="overview">
@@ -31,29 +38,38 @@ const Overview = () => {
               <StatsDisplay 
                     title="Antal byggnader" 
                     value={buildings.length}
-                    buttonText="Visa byggnader"
-                    useLink="/byggnader"
+                    // buttonText="Visa byggnader"
+                    // useLink="/byggnader"
                 />
                 <StatsDisplay 
                     title="Antal pågående uppgifter" 
                     value={myTasks.filter(t => t.status != "completed").length}
-                    buttonText="Visa pågående uppgifter"
+                    // buttonText="Visa pågående uppgifter"
                 />
                 <StatsDisplay 
                     title="Antal avklarade uppgifter" 
                     value={myTasks.filter(t => t.status === "completed").length}
-                    buttonText="Visa avklarade uppgifter"
+                    // buttonText="Visa avklarade uppgifter"
                 />
               </div>
 
-              <h2 className="overviewSubTitle">Dina byggnader</h2>
-              <BuildingsList buildings={buildings} variant="list--clean" />
+              <h2 
+                className={clsx(
+                  "overviewSubTitle", 
+                  {['overviewSubTitle--sm']: sm}
+                )}
+              >Dina byggnader</h2>
+              {!sm &&  <BuildingsList buildings={buildings} variant="list--clean" />}
+              {sm && <BuildingCardList buildings={buildings} />}
+
               { userRole === 'customer' && <button className="createBuildingButton" onClick={() => showModalVariant('createBuilding')} > <AddCircleIcon/> Skapa ny byggnad</button>}
 
               <JoinBuildingForm/>
 
-              <h2 className="overviewSubTitle">Dina uppgifter</h2>
-              <TaskListWithAccordion myTasks={myTasks} variant="list--clean" />
+              <h2 className={clsx("overviewSubTitle", {['overviewSubTitle--sm']: sm})}>Dina uppgifter</h2>
+
+              {!sm && <TaskListWithAccordion myTasks={myTasks} variant="list--clean" />}
+              {sm && <TaskCardList tasks={myTasks} />}
 
           </>
           }  
