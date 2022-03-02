6a3a10f824934edd80213fdf952c1ac73ea8df05
Linus Linde <linus.linde@bahnhof.se>
2022-03-02 09:47:20 +0100

    feat: Improve the menu to display options when the middle button is clicked

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,18 +1,39 @@
 import React from 'react'
+import AddIcon from '@mui/icons-material/Add';
 
 import './Menu.scss'
 
-const Menu = () => {
+import useBoolean from '../../hooks/useBoolean'
+import { useGlobal } from '../../providers/GlobalProvider';
+import { clsx } from '../../utils/utils';
+
+const Menu = ({  }) => {
+
+    const { showModalVariant, modalStatus } = useGlobal()
+
+    const [isOptionsVisible, { toggle: toggleOptions, off: hideOptions }] = useBoolean(undefined)
+
   return (
       <>
-        <div className="menu">
+        <div className={clsx("menu")}>
+            {isOptionsVisible && 
+                <div className="options">
+                     <p className="optionItem" onClick={() => (showModalVariant('joinBuilding'), hideOptions())}>Gå med i byggnad</p>
+                     <p className="optionItem" onClick={() => (showModalVariant('createBuilding'), hideOptions())}>Skapa byggnad</p>
+                 </div>
+            }
           <div className="innerMenu">
               <div className="testMenu"></div>
               <div className="menuButton leftButton">
                   <p>Byggnader</p>
               </div>
-              <div className="menuButton mainButton">
-                  <p>Översikt</p>
+              <div className="menuButton mainButton" onClick={() => toggleOptions(isOptionsVisible)}>
+                    <p>
+                        <AddIcon 
+                            className={clsx("menuAddIcon", {menuAddIconRotate: isOptionsVisible})}
+                        />
+                        {/* Verktyg */}
+                    </p>
               </div>
               <div className="menuButton rightButton">
                   <p>Uppgifter</p>
