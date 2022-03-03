906ce647e01552ab53beb9eb63cd1020e029bffb
Linus Linde <linus.linde@bahnhof.se>
2022-03-03 10:54:39 +0100

    feat: Update menu to dispaly different stuff depending on the page

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,17 +1,30 @@
-import React from 'react'
-import AddIcon from '@mui/icons-material/Add';
+import React, { useState, useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+import AddIcon from '@mui/icons-material/Add'
 
 import './Menu.scss'
 
 import useBoolean from '../../hooks/useBoolean'
-import { useGlobal } from '../../providers/GlobalProvider';
-import { clsx } from '../../utils/utils';
+import { useGlobal } from '../../providers/GlobalProvider'
+import { clsx } from '../../utils/utils'
 
 const Menu = ({  }) => {
 
-    const { showModalVariant, modalStatus } = useGlobal()
+    const location = useLocation()
+    const path = location.pathname
+
+    const { showModalVariant, modalStatus, modalVariant } = useGlobal()
+
+    const [isBuildingPage, setIsBuildingPage] = useState(undefined)
 
     const [isOptionsVisible, { toggle: toggleOptions, off: hideOptions }] = useBoolean(undefined)
+
+    useEffect(() => {
+        if(!path) return
+
+        if(path.includes("byggnad")) setIsBuildingPage(true)
+
+    }, [path])
 
   return (
       <>
@@ -25,7 +38,9 @@ const Menu = ({  }) => {
           <div className="innerMenu">
               <div className="testMenu"></div>
               <div className="menuButton leftButton">
-                  <p>Byggnader</p>
+                  
+                  {isBuildingPage && modalStatus !== 'SHOWN' && <p>Tillbaka</p>}
+                  {modalVariant === 'viewTask' && <p>Avbryt</p>}
               </div>
               <div className="menuButton mainButton" onClick={() => toggleOptions(isOptionsVisible)}>
                     <p className={clsx("menuAddIcon", {menuAddIconRotate: isOptionsVisible})}>
@@ -33,7 +48,9 @@ const Menu = ({  }) => {
                     </p>
               </div>
               <div className="menuButton rightButton">
-                  <p>Uppgifter</p>
+                  {modalVariant === 'viewTask' && <p>Antag</p>}
+                  {modalStatus !== 'SHOWN' && !isBuildingPage && <p>Uppgifter</p>}
+                  {isBuildingPage && modalStatus !== 'SHOWN' && <p>Knapp 2</p>}
               </div>
           </div>
         </div>
