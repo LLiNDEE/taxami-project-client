a5e2999e1af7c6b0236ebaa2b54de7d1afa34d2c
LLiNDEE <linus.linde@bahnhof.se>
2022-03-03 16:53:09 +0100

    feat: Fix colors on left and right button when viewTask modal is shown

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,19 +1,22 @@
 import React, { useState, useEffect } from 'react'
-import { useLocation } from 'react-router-dom'
+import { useLocation, Link } from 'react-router-dom'
 import AddIcon from '@mui/icons-material/Add'
 
 import './Menu.scss'
 
 import useBoolean from '../../hooks/useBoolean'
 import { useGlobal } from '../../providers/GlobalProvider'
+import { useMenu } from '../../providers/MenuProvider'
 import { clsx } from '../../utils/utils'
 
 const Menu = ({  }) => {
 
     const location = useLocation()
     const path = location.pathname
+    
+    const {  } = useMenu()
 
-    const { showModalVariant, modalStatus, modalVariant } = useGlobal()
+    const { showModalVariant, modalStatus, modalVariant, hideModal } = useGlobal()
 
     const [isBuildingPage, setIsBuildingPage] = useState(undefined)
 
@@ -36,18 +39,17 @@ const Menu = ({  }) => {
                  </div>
             }
           <div className="innerMenu">
-              <div className="testMenu"></div>
-              <div className="menuButton leftButton">
+              <div className={clsx("menuButton leftButton", {cancelButton: modalVariant === 'viewTask'})}>
                   
-                  {isBuildingPage && modalStatus !== 'SHOWN' && <p>Tillbaka</p>}
-                  {modalVariant === 'viewTask' && <p>Avbryt</p>}
+                  {isBuildingPage && modalStatus !== 'SHOWN' && <Link to="/oversikt" className="link">Tillbaka</Link>}
+                  {modalVariant === 'viewTask' && <p onClick={hideModal}>Avbryt</p>}
               </div>
               <div className="menuButton mainButton" onClick={() => toggleOptions(isOptionsVisible)}>
                     <p className={clsx("menuAddIcon", {menuAddIconRotate: isOptionsVisible})}>
                         <AddIcon />
                     </p>
               </div>
-              <div className="menuButton rightButton">
+              <div className={clsx("menuButton rightButton", {acceptButton: modalVariant === 'viewTask'})}>
                   {modalVariant === 'viewTask' && <p>Antag</p>}
                   {modalStatus !== 'SHOWN' && !isBuildingPage && <p>Uppgifter</p>}
                   {isBuildingPage && modalStatus !== 'SHOWN' && <p>Knapp 2</p>}
