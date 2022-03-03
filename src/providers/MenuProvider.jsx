d996fac7cc05202fec16e0be0c48e85a5842c651
LLiNDEE <linus.linde@bahnhof.se>
2022-03-03 22:14:45 +0100

    fix: Implement useUserTakeTask-hook. Still need to fix so the page refreshes on executes

--- src/providers/MenuProvider.jsx
+++ src/providers/MenuProvider.jsx
@@ -1,4 +1,7 @@
 import React, { useState, useEffect, useContext, createContext } from 'react'
+import { useParams } from 'react-router-dom'
+
+import useUserTakeTask from '../api/useUserTakeTask'
 
 const contextMenu = createContext({})
 
@@ -9,8 +12,17 @@ const MenuProvider = ({ children }) => {
      * Eventuellt göra en provider som håller all api-hooks
      */
 
+    const { execute: executeTakeTask, isSuccess: takeTaskSuccess, isError: takeTaskError } = useUserTakeTask()
+
+    useEffect(() => {
+        if(!takeTaskSuccess) return
+
+        console.log("Take task success", takeTaskSuccess)
+
+    },[takeTaskSuccess])
+
     return (
-        <contextMenu.Provider value={{}}>
+        <contextMenu.Provider value={{ executeTakeTask }}>
             {children}
         </contextMenu.Provider>
     )
