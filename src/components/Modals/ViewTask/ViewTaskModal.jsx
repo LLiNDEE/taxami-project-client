087e96ea2355b1c3d4f8262d3767234ebcb20a98
LLiNDEE <linus.linde@bahnhof.se>
2022-03-03 16:53:21 +0100

    feat: Implement takeTaskForm

--- src/components/Modals/ViewTask/ViewTaskModal.jsx
+++ src/components/Modals/ViewTask/ViewTaskModal.jsx
@@ -1,17 +1,33 @@
 import React, { useState } from 'react';
 
 import Modal from '../../core/Modal/Modal'
+import TakeTaskForm from '../../TakeTaskForm/TakeTaskForm';
 import { useGlobal } from '../../../providers/GlobalProvider';
 import { useData } from '../../../providers/DataProvider';
+import { useEffect } from 'react';
+
+const initialTakeTaskDetails = {
+  estimated_cost: "",
+  estimated_time: "",
+  optional_comment: "",
+}
 
 
 const ViewTaskModal = () => {
 
+  const { modalData } = useGlobal()
+
+  const [data, setData] = useState(initialTakeTaskDetails)
+
+  useEffect(() => {
+    console.log("modal data--->", modalData)
+  },[modalData])
+
   return (
     <Modal
         variant="default"
-        modalTitle="<--- TASK --->"
-        content={<p>Här visas task</p>}
+        modalTitle={modalData.title}
+        content={<TakeTaskForm {...{data, setData}}/>}
     /> 
   );
 };
