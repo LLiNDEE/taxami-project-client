a277f41844407a54fed2c60de6da323577175a64
Linus Linde <linus.linde@bahnhof.se>
2022-03-08 12:20:12 +0100

    fix: Implement yup and react-hook-form with inputs. Also set defaultValue to the user's current first/lastname and email

--- src/components/UpdateCredentialsForm/UpdateCredentialsForm.jsx
+++ src/components/UpdateCredentialsForm/UpdateCredentialsForm.jsx
@@ -8,6 +8,7 @@ import Input from '../core/Input/Input'
 const Schema = yup.object().shape({
     first_name: yup.string(),
     last_name: yup.string(),
+    email: yup.string().email(),
 })
 
 const UpdateCredentialsForm = ({ credentials }) => {
@@ -17,11 +18,31 @@ const UpdateCredentialsForm = ({ credentials }) => {
         resolver: yupResolver(Schema)
     })
 
+    const onSubmit = data => console.log(data)
+
   return (
     <div>
-        <Input label="Förnamn"/>
-        <Input label="Efternamn" />
-        <Input label="Epost" />
+      <form onSubmit={handleSubmit(onSubmit)} >
+          <Controller
+              name="first_name"
+              defaultValue={credentials.first_name}
+              control={control}
+              render={({ field }) => <Input label="Förnamn" {...field} ref={null} />}
+          />
+          <Controller
+            name="last_name"
+            defaultValue={credentials.last_name}
+            control={control}
+            render={({ field }) => <Input label="Efternamn" {...field} ref={null} />}
+          />
+          <Controller
+            name="email"
+            defaultValue={credentials.email}
+            control={control}
+            render={({ field }) => <Input label="Epost"  {...field}  ref={null} />}
+          />
+          <button>Spara</button>
+      </form>
     </div>
   )
 }
