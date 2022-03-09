134f33922cf7cbb8ad61290e3f73ab95187cb35a
Linus Linde <linus.linde@bahnhof.se>
2022-03-09 09:12:02 +0100

    fix: Fix so it hides the link to /mittkonto on mobile-view

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,17 +7,22 @@ import Chip from '../core/Chip/Chip';
 import Flex from '../core/Flex/Flex';
 
 import useAuth from '../../hooks/useAuth'
+import useBreakpoint from '../../hooks/useBreakpoint'
 
 const Header = () => {
 
     const auth = useAuth()
 
+    const { sm } = useBreakpoint()
+
     return (
         <header>
             <h2 className="headerTitle"><Link to="/oversikt">Taxamiiiiiiii</Link></h2>
+            {!sm && 
             <Flex justify="right">
                 <Link to="/mittkonto"><Chip className="myAccountChip" labelText="Mitt konto"/></Link>
             </Flex>
+            }
             <div className="logoutButtonContainer">
                 <button className="logOutbutton" onClick={() => auth.logoutUser()}>Logga ut <LogoutIcon className="logoutIcon"/> </button>
             </div>
