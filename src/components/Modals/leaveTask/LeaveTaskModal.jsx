736b8696179fd10dc09278d28f2120c678a6e0b1
Linus Linde <linus.linde@bahnhof.se>
2022-03-15 11:17:08 +0100

    fix: Fix so it hides buttons on mobile.

--- src/components/Modals/leaveTask/LeaveTaskModal.jsx
+++ src/components/Modals/leaveTask/LeaveTaskModal.jsx
@@ -1,19 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Modal from '../../core/Modal/Modal'
+import useBreakpoint from '../../../hooks/useBreakpoint'
 import { useGlobal } from '../../../providers/GlobalProvider';
 import { useData } from '../../../providers/DataProvider';
 
 
 const LeaveTaskModal = () => {
 
+    const { sm } = useBreakpoint()
+    
     const { userID } = useGlobal()
-    const {selectedTaskID, hideModal, leaveTask } = useData()
+    const {selectedTaskID, hideModal, leaveTask, setModalData } = useData()
+
+    useEffect(() => {
+      setModalData({task_id: selectedTaskID})
+    },[])
     
 
   return (
     <Modal
-        variant="yesNO"
+        variant={sm ? "default" : "yesNO"}
         modalTitle="Lämna"
         content={<p>Är du säker på att du vill lämna den här uppgiften?</p>}
         yesText="Ja"
